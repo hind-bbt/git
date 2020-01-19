8d1f39a14245d5c7dd391addf06d542c0514e99f
ahmed <chemlal.amd158@gmail.com>
2020-01-19 15:10:53 +0100

    hello

--- contact.php
+++ contact.php
@@ -2,3 +2,4 @@
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque hic animi sapiente enim dolor earum officia voluptates officiis itaque! Laboriosam quis pariatur dolorem mollitia consequuntur nam. Distinctio iste nostrum laboriosam, corporis ex labore iure odio. Optio nobis sint voluptas architecto magni et, quaerat ducimus, explicabo reiciendis doloribus error, reprehenderit voluptatem.</p>
 <h2 class="h2">hello</h2>
 <p class="h2">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni, quibusdam?</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, iure.</p>
