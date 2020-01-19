770efc2ba5cee3092f924e3790102c4758cbc41c
ahmed <chemlal.amd158@gmail.com>
2020-01-19 12:22:41 +0100

    new update on contact

--- contact.php
+++ contact.php
@@ -1,1 +1,2 @@
 <h1>contact page</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque hic animi sapiente enim dolor earum officia voluptates officiis itaque! Laboriosam quis pariatur dolorem mollitia consequuntur nam. Distinctio iste nostrum laboriosam, corporis ex labore iure odio. Optio nobis sint voluptas architecto magni et, quaerat ducimus, explicabo reiciendis doloribus error, reprehenderit voluptatem.</p>
