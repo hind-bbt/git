17f0499bc9313d00d04108566bb36b5f9ddd37ea
ahmed <chemlal.amd158@gmail.com>
2020-01-19 13:11:53 +0100

    add text

--- contact.php
+++ contact.php
@@ -1,2 +1,4 @@
 <h1>contact page</h1>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque hic animi sapiente enim dolor earum officia voluptates officiis itaque! Laboriosam quis pariatur dolorem mollitia consequuntur nam. Distinctio iste nostrum laboriosam, corporis ex labore iure odio. Optio nobis sint voluptas architecto magni et, quaerat ducimus, explicabo reiciendis doloribus error, reprehenderit voluptatem.</p>
+<h2>hello</h2>
+<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni, quibusdam?</p>
